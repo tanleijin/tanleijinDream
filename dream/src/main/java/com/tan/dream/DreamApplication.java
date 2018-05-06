f4788c844bc9180c547e4833891aacf318a36f6a
tlj <1016036277@qq.com>
2018-05-06 22:58:16 +0800

    1
    1

---
+++ dream/src/main/java/com/tan/dream/DreamApplication.java
@@ -0,0 +1,14 @@
+package com.tan.dream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableAutoConfiguration
+@SpringBootApplication
+public class DreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DreamApplication.class, args);
+	}
+}
---
+++ dream/src/main/java/com/tan/dream/test/testController.java
@@ -0,0 +1,24 @@
+package com.tan.dream.test;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * （描述）
+ *
+ * @Author tanleijin
+ * @date Create in 22:23 2018/5/6
+ */
+@Controller
+public class testController {
+
+    @GetMapping("test")
+    public String test(){
+
+        System.out.print("111111111111111");
+
+        return "test";
+    }
+
+}
